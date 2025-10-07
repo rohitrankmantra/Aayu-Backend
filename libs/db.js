d0372021e3f6f3842ffda40131f53a350d48b9f3
Rohit Rankmantra <rohitrankmantra12@gmail.com>
2025-10-07 15:29:05 +0530

    changes in the mongodb

--- libs/db.js
+++ libs/db.js
@@ -6,10 +6,7 @@ const connectDB = async () => {
     if (!process.env.MONGO_URI) {
       throw new Error('MONGO_URI is not defined in .env');
     }
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
